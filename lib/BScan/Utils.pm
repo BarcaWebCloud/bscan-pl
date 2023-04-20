131da37539e992ca3721652018e6d31f97c4261a
ahsouza <annibalhsouza@gmail.com>
2023-04-20 05:10:41 -0300

    chore: init bscan script perl

---
+++ lib/BScan/Utils.pm
@@ -0,0 +1,38 @@
+package BScan::Utils;
+#     BScan::Utils - some useful command-line utilities
+#           Copyright (C) 2021 - 2023, Barca, Inc. 
+#    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+#    Project: BSCAN to scanner MotherBoards. CPU, Memory Ram, SO and more
+ 
+ our $VERSION = 0.052;
+ 
+ =head1 NAME
+ 
+ App::Utils - some useful command-line utilities
+ 
+ =head1 DESCRIPTION
+ 
+ A Script For Getting Information on Hardware, Software and Atmospheres for Perl
+ 
+ F<bscan> - print a date-time tag in form C<YYYY-MM-DD-hhmmss>
+ 
+ 
+ =head1 AUTHOR
+ 
+ Barca Web Cloud opensource@barca.com https://cloud.barca.com/
+ 
+ =head1 INSTALLATION
+ 
+ Using C<cpan>:
+ 
+  $ cpan BScan::Utils
+ 
+ Manual install:
+ 
+  $ perl Makefile.PL
+  $ make
+  $ make install
+ 
+ =cut
+ 
+ 1;
